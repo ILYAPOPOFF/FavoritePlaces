ecfc176e6a4d9a2afb3a5015274a164645fcc2d4
ILYAPOPOFF <IlyaP1998@gmail.com>
2023-09-24 22:04:59 +0400

    first commit

---
+++ FavoritePlaces/FavoritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritePlacesApp.swift
+//  FavoritePlaces
+//
+//  Created by ILYA POPOV on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
